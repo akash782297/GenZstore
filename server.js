0324c0960d46c820e1de79009c8cdb863db0989a
akash782297 <akashdubey6700@gmail.com>
2025-08-26 12:26:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,26 +6,23 @@ const path = require('path');
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Path ko wapas theek kiya gaya hai
+// Path बिल्कुल सही है
 app.use(express.static(path.join(__dirname, 'public')));
 
-// DB Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.error(err));
 
-// Path ko wapas theek kiya gaya hai
+// Path बिल्कुल सही है
 app.use('/api/products', require('./routes/productRoutes'));
 
-// Path ko wapas theek kiya gaya hai
+// Path बिल्कुल सही है
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
