602480cab425b019df7eb5a4d4cd49f271fd96aa
akash782297 <akashdubey6700@gmail.com>
2025-08-26 09:50:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.error(err));
+
+app.use('/api/products', require('./routes/productRoutes'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
